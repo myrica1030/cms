4011f6cd724034d6e6642be1eb0f2d432693db8d
myrica1030 <imutoe@outlook.com>
2024-09-01 06:07:52 +0800

    ci(page-builder): setup component test

--- apps/cms-page-builder/cypress.config.ts
+++ apps/cms-page-builder/cypress.config.ts
@@ -3,10 +3,19 @@ import viteConfig from './vite.config'
 
 export default defineConfig({
   viewportWidth: 1200,
+  reporter: 'cypress-multi-reporters',
+
+  video: false,
 
   e2e: {
     setupNodeEvents(_on, _config) {},
     baseUrl: 'http://localhost:5173',
+    reporterOptions: {
+      reporterEnabled: 'list, mocha-junit-reporter',
+      mochaJunitReporterReporterOptions: {
+        mochaFile: 'cypress/reports/junit-e2e.xml',
+      },
+    },
   },
 
   component: {
@@ -14,6 +23,12 @@ export default defineConfig({
       framework: 'vue',
       bundler: 'vite',
       viteConfig,
+    },
+    reporterOptions: {
+      reporterEnabled: 'list, mocha-junit-reporter',
+      mochaJunitReporterReporterOptions: {
+        mochaFile: 'cypress/reports/junit-component.xml',
+      },
     },
   },
 })
