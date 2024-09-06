3cf30572eb0b480d2c570f0c935f610525938c12
myrica1030 <imutoe@outlook.com>
2024-09-06 20:46:55 +0800

    chore: replace JSONSchema decorator to ApiProperty

--- apps/cms-api/src/article/dto/create-article.dto.ts
+++ apps/cms-api/src/article/dto/create-article.dto.ts
@@ -1,10 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { Prisma } from '@prisma/client'
 import { IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from 'class-validator'
-import { JSONSchema } from 'class-validator-jsonschema'
 import { IsIdProperty, IsRichTextProperty } from 'common/decorator/api-property.decorator'
 
 export class CreateArticleDto implements Omit<Prisma.ArticleUncheckedCreateInput, 'tags' | 'authorId'> {
-  @JSONSchema({ title: 'The title of the article', example: 'Lorem ipsum' })
+  @ApiProperty({ title: 'The title of the article', example: 'Lorem ipsum' })
   @IsString() @MaxLength(60)
   @IsNotEmpty()
   title: string
@@ -16,7 +16,7 @@ export class CreateArticleDto implements Omit<Prisma.ArticleUncheckedCreateInput
   @IsOptional()
   categoryId?: number
 
-  @JSONSchema({ title: 'The tag names of the article', example: ['semantic-ui', 'material-ui'] })
+  @ApiProperty({ title: 'The tag names of the article', example: ['semantic-ui', 'material-ui'] })
   @MinLength(1, { each: true }) @MaxLength(20, { each: true })
   @IsOptional()
   tags?: string[]
