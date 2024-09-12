db655bdcfec736a8c4f55b6c1f3e4bc3acfb73e4
myrica1030 <332019319@qq.com>
2024-09-12 18:03:48 +0800

    chore(api): add detailed comments for username and key field regex
    - Clarify allowed characters for USERNAME and KEY fields.
    - Improve code readability and maintainability.

--- apps/cms-api/src/_common/constants/regular-expressions.ts
+++ apps/cms-api/src/_common/constants/regular-expressions.ts
@@ -1,4 +1,6 @@
 export const REGEXP = {
+  /** The username only allows alphanumeric characters (0-9, a-z, A-Z), underscore (_), hyphen (-), and space */
   USERNAME: String.raw`^[\w. -]+$`,
+  /** The key field only allows alphanumeric characters (0-9, a-z, A-Z), and hyphen (-) */
   KEY: String.raw`^[\dA-Za-z-]+$`,
 } as const
