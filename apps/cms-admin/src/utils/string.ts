dc38f74725076ea9468fc94a83146b88a5214b61
myrica1030 <332019319@qq.com>
2024-09-13 00:45:14 +0800

    feat(admin): integrate vee-validate and yup for form validation
    - Add vee-validate for managing form state and validation.
    - Implement yup for schema-based validation of username and password fields.
    - Enhance user interface with aria attributes and error handling for improved accessibility.

--- apps/cms-admin/src/utils/string.ts
+++ apps/cms-admin/src/utils/string.ts
@@ -1,3 +1,17 @@
-export function capitalize(str: string): string {
+function camelToLowerWords(text: string): string {
+  return text
+    .split(/(?=[A-Z])/)
+    .map(word => word.trim())
+    .join(' ')
+    .toLowerCase()
+}
+
+export function capitalize(str: string | undefined): string {
+  if (!str) return ''
   return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+export function sentence(text: string): string {
+  const words = camelToLowerWords(text)
+  return capitalize(words)
 }
